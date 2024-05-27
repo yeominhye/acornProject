18cf6a7285d844711921eb882e1c0f88362de403
wonwhimsical <wonwhimsical@gmail.com>
2024-05-27 14:34:26 +0900

    게시판 상세 수정

--- project/src/main/webapp/resources/js/comment.js
+++ project/src/main/webapp/resources/js/comment.js
@@ -158,3 +158,19 @@ function delComment(btn) {
         });
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+	var userCode = document.querySelector('.userCode').value;   
+	
+    if (userCode === 'null') {
+        // #regComment 요소가 없는 경우 실행됩니다.
+        // .my_comment_sender 요소의 내용을 변경합니다.
+        var commentSender = document.querySelector('.my_comment_sender');
+        commentSender.textContent = "로그인 후 댓글을 입력하세요.";
+        commentSender.style.fontWeight="500";
+        
+        // .my_comment_content 요소의 placeholder를 변경합니다.
+        var commentTextarea = document.querySelector('.my_comment_content');
+        commentTextarea.placeholder = " ";
+    }
+});
