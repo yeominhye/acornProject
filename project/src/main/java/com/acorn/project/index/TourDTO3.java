a012c7c8bd0b76b8a0c265496864427988a901b2
sample <ghdtkq1235@gmail.com>
2024-06-02 12:04:54 +0900

    1

---
+++ project/src/main/java/com/acorn/project/index/TourDTO3.java
@@ -0,0 +1,22 @@
+package com.acorn.project.index;
+
+import org.json.JSONObject;
+
+import com.acorn.project.like.Like;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TourDTO3 {
+	
+	Object expguide;
+	Object infocenter;
+	Object restdate;
+	Object parking;
+	
+
+}
